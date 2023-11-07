a0f0a14906a4b025fded55082ff87d94aca61129
HPLinh <21520323@gm.uit.edu.vn>
2023-11-07 23:21:32 +0700

    fix and update homepage + update profile_page

--- app/src/main/java/com/example/proj_moneymanager/activities/Profile.java
+++ app/src/main/java/com/example/proj_moneymanager/activities/Profile.java
@@ -3,22 +3,25 @@ package com.example.proj_moneymanager.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.proj_moneymanager.ListAdapter;
+import com.example.proj_moneymanager.ProfileAdapter;
+import com.example.proj_moneymanager.Profile_Option;
 import com.example.proj_moneymanager.R;
 import com.example.proj_moneymanager.app.AppConfig;
-import com.example.proj_moneymanager.optProfile;
 
 import java.util.ArrayList;
 
 public class Profile extends AppCompatActivity {
     TextView textViewLogout;
     private AppConfig appConfig;
+
+    ListView lv_profileOption;
+    ArrayList<Profile_Option> arr_profileOption;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,27 +30,22 @@ public class Profile extends AppCompatActivity {
 
         setContentView(R.layout.profile);
 
-        /*int[] imageID = {R.drawable.icon_person, R.drawable.icon_lock, R.drawable.icon_switch, R.drawable.notification};
-        String[] label = {"Name", "Password", "Switch Account", "Notification"};
-        String[] labelInfo = {"Group03", "******", "group03@gmail.com", ""};
+        //Xử lý Profile Adapter cho listview
+        lv_profileOption = (ListView) findViewById(R.id.lv_optProfile);
+        arr_profileOption = new ArrayList<Profile_Option>();
 
-        ArrayList<optProfile> optProfileArrayList = new ArrayList<>();
+        arr_profileOption.add(new Profile_Option("Name", "Group03", R.drawable.icon_person_profile));
+        arr_profileOption.add(new Profile_Option("Password", "********", R.drawable.icon_lock));
+        arr_profileOption.add(new Profile_Option("Switch Account", "group03@gmail.com", R.drawable.icon_switch));
+        arr_profileOption.add(new Profile_Option("Notification", "", R.drawable.icon_notification_fill));
 
-        for (int i = 0; i < imageID.length; i++) {
-            optProfile opt = new optProfile(label[i], labelInfo[i], imageID[i]);
-            optProfileArrayList.add(opt);
-        }
+        ProfileAdapter profileAdapter = new ProfileAdapter(
+                Profile.this,
+                R.layout.profile,
+                arr_profileOption
+        );
+        lv_profileOption.setAdapter(profileAdapter);
 
-        ListAdapter listAdapter = new ListAdapter(this, optProfileArrayList);
-        ListView listView = findViewById(R.id.lv_optProfile);
-        listView.setAdapter(listAdapter);
-        listView.setClickable(true);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Xử lý sự kiện khi một mục trên ListView được nhấn
-            }
-        });*/
         //Xử lí Logout
         textViewLogout = (TextView) findViewById(R.id.textview_logout);
         textViewLogout.setOnClickListener(new View.OnClickListener() {
