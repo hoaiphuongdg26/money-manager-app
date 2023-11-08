2200d73e39f4bed418a9769302cf499a881c5912
ConGiansBay <truonggiangnsl123@gmail.com>
2023-11-08 17:19:44 +0700

    resolve conflict

--- app/src/main/java/com/example/proj_moneymanager/retrofit/ApiClient.java
+++ app/src/main/java/com/example/proj_moneymanager/retrofit/ApiClient.java
@@ -4,11 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-<<<<<<< HEAD
     private static final String BASE_URL ="http://172.16.0.221/money_management/";
-=======
-    private static final String BASE_URL ="http://10.0.138.95/money_management/";
->>>>>>> 00b01dac1cdda0ab600031230602168292f3a0c5
     private static Retrofit retrofit =null;
 
     public static Retrofit getApiClient() {
