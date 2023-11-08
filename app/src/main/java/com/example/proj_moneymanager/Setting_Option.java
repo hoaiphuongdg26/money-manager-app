00b01dac1cdda0ab600031230602168292f3a0c5
HPLinh <21520323@gm.uit.edu.vn>
2023-11-08 17:04:03 +0700

    fix profile_page (k hien thi duoc imgview + add footer.xml + add setting_page(xong)

--- app/src/main/java/com/example/proj_moneymanager/Setting_Option.java
+++ app/src/main/java/com/example/proj_moneymanager/Setting_Option.java
@@ -1,23 +1,17 @@
 package com.example.proj_moneymanager;
 
-public class Profile_Option {
-    public String label, labelInfo;
+public class Setting_Option {
+    public String label;
     public int imageOption;
 
-    public Profile_Option(String label, String labelInfo, int imageOption) {
+    public Setting_Option(String label, int imageOption) {
         this.label = label;
-        this.labelInfo = labelInfo;
         this.imageOption = imageOption;
     }
 
     public String getLabel() {
         return label;
     }
-
-    public String getLabelInfo() {
-        return labelInfo;
-    }
-
     public int getImageOption() {
         return imageOption;
     }
