1738f2941fb21547e24d7c8093f9094d1532646d
Hoai Phuong <intern@dotb.vn>
2023-12-17 23:34:16 +0700

    xử lý chọn tháng nhanh

--- app/src/main/java/com/example/proj_moneymanager/CalendarViewHolder.java
+++ app/src/main/java/com/example/proj_moneymanager/CalendarViewHolder.java
@@ -1,6 +1,8 @@
 package com.example.proj_moneymanager;
 
+import android.app.DatePickerDialog;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +12,7 @@ public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.
 
     public final TextView dayOfMonth;
     private final CalendarAdapter.OnItemListener onItemListener;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
 
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
     {
@@ -17,11 +20,13 @@ public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.
         dayOfMonth = itemView.findViewById(R.id.cellDayText);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view)
     {
+        ((ViewGroup) itemView.getParent()).removeView(itemView);
         onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
     }
 }
