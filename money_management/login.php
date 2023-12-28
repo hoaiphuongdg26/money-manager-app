1e72cea509129d96fe36de32a5195e1cb8a63554
Hoai Phuong <intern@dotb.vn>
2023-12-28 07:55:25 +0700

    vừa rv xong: file server

--- money_management/login.php
+++ money_management/login.php
@@ -1,35 +1,41 @@
 <?php
-$UserName = $_POST["UserName"];
-$Password = $_POST["Password"];
+if (isset($_POST["UserName"]) && isset($_POST["Password"])) {
+    $UserName = $_POST["UserName"];
+    $Password = $_POST["Password"];
 
-require 'DataBase.php';
-if($con){
-    $sql = "select * from user_information where UserName = '$UserName'";
-    $result = mysqli_query($con,$sql);
+    require 'DataBase.php';
+    if ($con) {
+        $sql = "select * from user_information where UserName = '$UserName'";
+        $result = mysqli_query($con, $sql);
 
-    if(mysqli_num_rows($result)>0){
-        $row = mysqli_fetch_assoc($result);
-        $dbPassword = $row['Password'];
-        if(md5($Password)==$dbPassword){
-            $status = "ok";
-            $result_code = 1;
-            $name = $row['FullName'];
-        echo json_encode(array('status'=>$status,'result_code'=>$result_code,'name'=>$name));
-        }
-        else{
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $dbPassword = $row['Password'];
+            if (md5($Password) == $dbPassword) {
+                $status = "ok";
+                $result_code = 1;
+                $userData = array(
+                    'userID' => $row['UserID'],
+                    'FullName' => $row['FullName'],
+                    'UserName' => $row['UserName'],
+                    'Password' => $row['Password'],
+                    'Email' => $row['Email'],
+                    'PhoneNumber' => $row['PhoneNumber']
+                );
+                echo json_encode(array('status' => $status, 'result_code' => $result_code, 'userData' => $userData));
+            } else {
+                $status = "ok";
+                $result_code = 0;
+                echo json_encode(array('status' => $status, 'result_code' => $result_code));
+            }
+        } else {
             $status = "ok";
             $result_code = 0;
-            echo json_encode(array('status'=>$status,'result_code'=>$result_code));
+            echo json_encode(array('status' => $status, 'result_code' => $result_code));
         }
+    } else {
+        $status = "failed";
+        echo json_encode(array('status' => $status), JSON_FORCE_OBJECT);
     }
-    else{
-        $status = "ok";
-        $result_code = 0;
-        echo json_encode(array('status'=>$status,'result_code'=>$result_code));
-    }
-}
-else{
-    $status = "failed";
-    echo json_encode(array('status'=>$status),JSON_FORCE_OBJECT);
 }
 ?>
